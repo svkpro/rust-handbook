a455c58e63ef23ac9db7a431122d56cca3fe863f
Vasili Solovei <vasilii.solovei@inturn.co>
2020-02-25 09:07:39 +0300

    Added the error handling

--- cli/rgrep/src/main.rs
+++ cli/rgrep/src/main.rs
@@ -1,21 +1,29 @@
+#![allow(dead_code)]
 use structopt::StructOpt;
 
-/// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
-    /// The pattern to look for
     pattern: String,
-    /// The path to the file to read
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
 fn main() {
     let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path).expect("Could not read file");
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    let content = std::fs::read_to_string(&args.path);
+    match content {
+        Ok(_) => {
+            let u_content = &content.unwrap();
+            for line in u_content.lines() {
+                if line.contains(&args.pattern) {
+                    println!("{}", line);
+                }
+            }
+            std::process::exit(0);
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
         }
     }
 }
